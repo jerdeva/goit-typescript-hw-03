5999090876fb99ea15bdddbc429eafd5c8c69888
Olena Jerdeva <lena.jerdeva28@gmail.com>
2023-10-26 19:17:40 +0300

    HW 1.ts

--- src/1.ts
+++ src/1.ts
@@ -4,12 +4,9 @@
   напишіть скорочену ініціалізацію.
 */
 
-class Student {
-  public name: string;
-  public age: number;
-  public grade: string;
 
-  constructor(name: string, age: number, grade: string) {
+class Student {
+  constructor(public name: string, public age: number, public grade: string) {
     this.name = name;
     this.age = age;
     this.grade = grade;
